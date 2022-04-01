821083c9aba0331163126443763aeac403c03ed1
sramk <sriram.kannan@my.jcu.edu.au>
2022-04-01 10:21:44 +0800

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,11 +1,17 @@
 def main():
-    password = input("Please enter your password: ")
+    password = get_password()
     length = len(password)
     while length <= 4:
         print("Error! Password is too short!")
-        password = input("Please enter your password: ")
+        password = get_password()
         length = len(password)
     print(string_to_asterisks(length))
+
+
+def get_password():
+    password = input("Please enter your password: ")
+    return password
+
 
 def string_to_asterisks(length):
     return "*" * length
