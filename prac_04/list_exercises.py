9b7cb03c0d781679ceda8b192a05a1f6f885e645
sramk <sriram.kannan@my.jcu.edu.au>
2022-04-08 10:06:22 +0800

    Update list_exercises.py in prac_04

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,3 +1,4 @@
+# 1
 numbers = []
 total_numbers = 5
 for i in range(total_numbers):
@@ -9,3 +10,12 @@ print(f"The last number is: {numbers[-1]}")
 print(f"The smallest number is: {min(numbers)}")
 print(f"The largest number is: {max(numbers)}")
 print(f"The average of the numbers is: {(min(numbers) + max(numbers) / 2)}")
+
+# 2
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter your username: ")
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
