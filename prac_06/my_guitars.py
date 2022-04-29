2596c9fd38d465e3e8e76c2b7cac531d1d3088c3
sramk <sriram.kannan@my.jcu.edu.au>
2022-04-29 10:40:55 +0800

    Create class Guitars to add name, year and cost of guitar to a guitar object

---
+++ prac_06/my_guitars.py
@@ -0,0 +1,22 @@
+class Guitars:
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initial variables for the class"""
+
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return "{} ({}) : ${:,}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        age = 2022 - self.year
+        return age
+
+    def is_vintage(self):
+        if self.get_age() > 50:
+            return True
+        else:
+            return False
+
