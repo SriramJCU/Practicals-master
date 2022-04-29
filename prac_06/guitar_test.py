3aac59582f1b270cea450526a40582dc92ddc829
sramk <sriram.kannan@my.jcu.edu.au>
2022-04-29 11:14:31 +0800

    Create client program for class Guitars in guitars.py

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,26 +1,10 @@
-from my_guitars import Guitars
+from guitar import Guitars
 
 first_guitar = Guitars("Gibson L-5 CES", 1922, 16035.40)
 second_guitar = Guitars("Frostman L-8 EFG", 2013, 4058.48)
 third_guitar = Guitars("Spectre L-15 MSE", 1899, 50289.55)
 
-guitar_collection = [first_guitar, second_guitar, third_guitar]
-
-print("My guitars!")
-name = input("Name: ")
-while name != "":
-    year = int(input("Year: "))
-    cost = float(input("Cost: $"))
-    guitar_collection.append(Guitars(name, year, cost))
-    print(f"{name} ({year}) : ${cost} added.")
-    name = input("Name: ")
-
-print("These are my guitars:")
-i = 0
-for guitar in guitar_collection:
-    i = guitar_collection.index(guitar)
-    if guitar.is_vintage():
-        vintage_string = "(vintage)"
-    else:
-        vintage_string = ""
-    print("Guitar {}: {:>20} ({}), worth ${:10,.2f} {}".format((i+1), guitar.name, guitar.year, int(guitar.cost), vintage_string))
+print(f"{first_guitar.name} get_age() - Expected 100, Got {first_guitar.get_age()}")
+print(f"{second_guitar.name} get_age() - Expected 9, Got {second_guitar.get_age()}")
+print(f"{first_guitar.name} is_vintage() - Expected True, Got {first_guitar.is_vintage()}")
+print(f"{second_guitar.name} is_vintage() - Expected False, Got {second_guitar.is_vintage()}")
---
+++ prac_06/guitars.py
@@ -0,0 +1,26 @@
+from guitar import Guitars
+
+first_guitar = Guitars("Gibson L-5 CES", 1922, 16035.40)
+second_guitar = Guitars("Frostman L-8 EFG", 2013, 4058.48)
+third_guitar = Guitars("Spectre L-15 MSE", 1899, 50289.55)
+
+guitar_collection = [first_guitar, second_guitar, third_guitar]
+
+print("My guitars!")
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    guitar_collection.append(Guitars(name, year, cost))
+    print(f"{name} ({year}) : ${cost} added.")
+    name = input("Name: ")
+
+print("These are my guitars:")
+i = 0
+for guitar in guitar_collection:
+    i = guitar_collection.index(guitar)
+    if guitar.is_vintage():
+        vintage_string = "(vintage)"
+    else:
+        vintage_string = ""
+    print("Guitar {}: {:>20} ({}), worth ${:10,.2f} {}".format((i+1), guitar.name, guitar.year, int(guitar.cost), vintage_string))
