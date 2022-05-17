0001c47fc243c120382ba49409f008af54364b19
SriramJCU <sriram.kannan@my.jcu.edu.au>
2022-05-17 16:47:21 +0800

    Create class SilverServiceTaxi and perfom tests

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,9 @@
+from silver_service_taxi import SilverServiceTaxi
+
+"""Test code for class SilverServiceTaxi"""
+
+fancy_taxi = SilverServiceTaxi('Benz', 100, 2)
+fancy_taxi.drive(18)
+
+print(fancy_taxi)
+print(fancy_taxi.get_fare())
