e131d08564fbb515317530701179df192662b86c
SriramJCU <sriram.kannan@my.jcu.edu.au>
2022-05-17 09:23:47 +0800

    create lines of code based on class Taxi

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,26 @@
+from taxi import Taxi
+
+# 1
+
+new_taxi = Taxi("Prius 1", 100, 1.23)
+
+# 2
+
+new_taxi.drive(40)
+
+# 3
+
+print("{}, Current fare: {}".format(new_taxi, new_taxi.get_fare()))
+
+# 4
+
+new_taxi.start_fare()
+
+# 5
+
+new_taxi.drive(100)
+
+# 6
+
+print("{}, Current fare: {}".format(new_taxi, new_taxi.get_fare()))
+
